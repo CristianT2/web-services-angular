bdeae9b6a3dedb76417d32df8d882472684570cc
CristianT2 <c.torrejon96@gmail.com>
2025-05-18 22:56:03 -0300

    Se creo el servicio y el componente con las configuraciones de conexion con la api de noticias

--- src/app/noticias/noticias.component.ts
+++ src/app/noticias/noticias.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NoticiasService } from '../services/noticias.service';
 
 @Component({
   selector: 'app-noticias',
@@ -6,6 +7,25 @@ import { Component } from '@angular/core';
   templateUrl: './noticias.component.html',
   styleUrl: './noticias.component.css'
 })
-export class NoticiasComponent {
+export class NoticiasComponent implements OnInit{
 
+  noticias: any[] = [];
+
+  constructor(private noticiasService: NoticiasService){}
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  //Se obtienen la noticias obtenidas de la api
+  public obtenerNoticias(): void{
+    this.noticiasService.getNoticias().subscribe(
+      (response: any) => {
+        this.noticias = response.homepageArticles[0].articles.slices(0,10);
+      },
+      (error: any) => {
+        console.log("Error al obtener las noticias", error);
+      }
+    )
+  }
 }
